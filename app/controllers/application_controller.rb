9ed2ef9af4183f9a814cf216afebf0a61c30a6c9
tylerhuimingalone <tylermingalone@gmail.com>
2019-10-31 17:42:20 -0400

    Trying to fix heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :gender, :email, :password])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:first_name, :last_name, :gender, :email, :password])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :gender, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :gender, :email, :password, :phone_number])
   end
 end
