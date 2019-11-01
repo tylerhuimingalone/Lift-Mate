921b3c348cdf7b7be26734aecd1504e1aade65a8
tylerhuimingalone <tylermingalone@gmail.com>
2019-11-01 16:30:14 -0400

    sidekiq initializer

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,11 @@
+if Rails.env.development?
+  require 'sidekiq/testing'
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDISTOGO_URL'] }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDISTOGO_URL'] }
+end
